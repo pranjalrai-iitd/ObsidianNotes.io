a208b90cf2b09552819ffe8442e4dea19d7a6674
Pranjal Rai <pranjalraiiitd@gmail.com>
2025-06-25 23:13:45 +0530

    Jun25

--- source/quartz.config.ts
+++ source/quartz.config.ts
@@ -23,9 +23,9 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "JetBrains Mono",
-        body: "JetBrains Mono",
-        code: "JetBrains Mono"
+        header: "JetBrains",
+        body: "JetBrains",
+        code: "JetBrains"
       },
       colors: {
         lightMode: {
