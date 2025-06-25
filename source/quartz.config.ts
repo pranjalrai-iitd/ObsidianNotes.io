aca29b7f5ae654b7c9b5dff71ebd572bb3afc3c3
Pranjal Rai <pranjalraiiitd@gmail.com>
2025-06-25 23:17:35 +0530

    Jun25

--- source/quartz.config.ts
+++ source/quartz.config.ts
@@ -24,7 +24,7 @@ const config: QuartzConfig = {
       cdnCaching: true,
       typography: {
         header: "JetBrains Mono",
-        body: "JetBrains Mono",
+        body: "Playfair Display",
         code: "JetBrains Mono"
       },
       colors: {
