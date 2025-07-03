04aed04ddd7b267fd965afa2cfe57dde4f4c637b
pranjalrai-iitd <pranjalrai-iitd@users.noreply.github.com>
2025-07-03 19:46:18 +0530

    font

--- source/quartz.config.ts
+++ source/quartz.config.ts
@@ -23,7 +23,7 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "JetBrains Mono",
+        header: "Lora",
         body: "Lora",
         code: "Lora"
       },
