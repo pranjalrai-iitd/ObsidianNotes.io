65f46bcfe7e079f000b9ef1111470c13cad4432a
pranjalrai-iitd <pranjalrai-iitd@users.noreply.github.com>
2025-07-03 19:38:52 +0530

    3Jul

--- source/quartz.config.ts
+++ source/quartz.config.ts
@@ -24,7 +24,7 @@ const config: QuartzConfig = {
       cdnCaching: true,
       typography: {
         header: "JetBrains Mono",
-        body: "JetBrains Mono",
+        body: "Lora",
         code: "JetBrains Mono"
       },
       colors: {
