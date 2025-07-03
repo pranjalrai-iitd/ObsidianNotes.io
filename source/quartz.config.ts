80995815a71c84cb44702eee115c862585817c4b
pranjalrai-iitd <pranjalrai-iitd@users.noreply.github.com>
2025-07-03 19:58:17 +0530

    font

--- source/quartz.config.ts
+++ source/quartz.config.ts
@@ -23,9 +23,9 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Fira Code",
-        body: "Fira Code",
-        code: "Fira Code"
+        header: "JetBrains Mono",
+        body: "JetBrains Mono",
+        code: "JetBrains Mono"
       },
       colors: {
         lightMode: {
